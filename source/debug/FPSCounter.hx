aeec8be7a4f48ff91f0717a17d44d497dd0c2713
MTGC <satriyoadi130@gmail.com>
2026-01-01 12:05:41 +0700

    Revert "wuh"
    This reverts commit 7529e24dcf8af762ebb8c427e9f9267d4f2ee9ec.

--- source/debug/FPSCounter.hx
+++ source/debug/FPSCounter.hx
@@ -35,33 +35,16 @@ class FPSCounter extends TextField
 	@:noCompletion private var times:Array<Float>;
 
 	private var os:String = "";
-	private var timeIndex:Int = 0;
-    private var timeBuffer:Array<Float> = [];
 
-	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000) {
+	public function new(x:Float = 10, y:Float = 10, color:Int = 0x000000)
+	{
 		super();
-		
-		#if !officialBuild
-		var platformInfo:String = LimeSystem.platformName;
-		
-		#if cpp
-		platformInfo += ' ${getArch()}';
-		#end
-		
-		#if debug
-		platformInfo += ' (Debug)';
-		#elseif release
-		platformInfo += ' (Release)';
-		#end
-		
-		os = '\nOS: $platformInfo';
 
-		background = true;
-        backgroundColor = 0x66000000;
-		
-		if (LimeSystem.platformVersion != null && LimeSystem.platformVersion != LimeSystem.platformName) {
-			os += ' - ${LimeSystem.platformVersion}';
-		}
+		#if !officialBuild
+		if (LimeSystem.platformName == LimeSystem.platformVersion || LimeSystem.platformVersion == null)
+			os = '\nOS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end;
+		else
+			os = '\nOS: ${LimeSystem.platformName}' #if cpp + ' ${getArch() != 'Unknown' ? getArch() : ''}' #end + ' - ${LimeSystem.platformVersion}';
 		#end
 
 		positionFPS(x, y);
@@ -75,73 +58,38 @@ class FPSCounter extends TextField
 		text = "FPS: ";
 
 		times = [];
-        timeBuffer = [for (i in 0...FlxG.updateFramerate) 0.0];
 	}
 
 	var deltaTimeout:Float = 0.0;
-	public var peakMemoryMegas(default, null):Float = 0;
-    private var memoryUpdateTimer:Float = 0;
 
-	private override function __enterFrame(deltaTime:Float):Void {
+	// Event Handlers
+	private override function __enterFrame(deltaTime:Float):Void
+	{
+		// prevents the overlay from updating every frame, why would you need to anyways
 		if (deltaTimeout > 1000) {
 			deltaTimeout = 0.0;
 			return;
 		}
 
 		final now:Float = haxe.Timer.stamp() * 1000;
+		times.push(now);
+		while (times[0] < now - 1000) times.shift();
 
-		timeBuffer[timeIndex] = now;
-		timeIndex = (timeIndex + 1) % FlxG.updateFramerate;
-
-		var count:Int = 0;
-		for (i in 0...FlxG.updateFramerate) {
-			if (now - timeBuffer[i] < 1000 && timeBuffer[i] > 0) {
-				count++;
-			}
-		}
-
-		memoryUpdateTimer += deltaTime;
-		if (memoryUpdateTimer > 500) {
-			memoryUpdateTimer = 0;
-			if (memoryMegas > peakMemoryMegas) {
-				peakMemoryMegas = memoryMegas;
-			}
-		}
-		
+		currentFPS = times.length < FlxG.updateFramerate ? times.length : FlxG.updateFramerate;		
 		updateText();
 		deltaTimeout += deltaTime;
 	}
 
-	public dynamic function updateText():Void {
-		var frameTime:Float = 1000 / (currentFPS > 0 ? currentFPS : 1);
-		var fpsWarning:String = "";
-        var memoryWarning:String = "";
-
-		if (currentFPS < FlxG.drawFramerate * 0.5) {
-			fpsWarning = " [LOW]";
-			textColor = 0xFFFF0000;
-		} else if (currentFPS < FlxG.drawFramerate * 0.8) {
-			fpsWarning = " [WARNING]";
-			textColor = 0xFFFFFF00;
-		} else {
-			textColor = 0xFF00FF00;
-		}
-
-		if (memoryMegas > 1.5 * 1024 * 1024 * 1024) {
-			memoryWarning = " [HIGH]";
-			textColor = 0xFFFF0000;
-		}
-		
+	public dynamic function updateText():Void // so people can override it in hscript
+	{
 		text = 
-		'FPS: $currentFPS$fpsWarning' + 
-		'\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}$memoryWarning' +
+		'FPS: $currentFPS' + 
+		'\nMemory: ${flixel.util.FlxStringUtil.formatBytes(memoryMegas)}' +
 		os;
 
 		textColor = 0xFFFFFFFF;
 		if (currentFPS < FlxG.drawFramerate * 0.5)
 			textColor = 0xFFFF0000;
-		else if (currentFPS < FlxG.drawFramerate * 0.8)
-			textColor = 0xFFFFFF00;
 	}
 
 	inline function get_memoryMegas():Float
@@ -151,14 +99,6 @@ class FPSCounter extends TextField
 		scaleX = scaleY = #if android (scale > 1 ? scale : 1) #else (scale < 1 ? scale : 1) #end;
 		x = FlxG.game.x + X;
 		y = FlxG.game.y + Y;
-	}
-
-	public function toggle(visible:Bool):Void {
-        this.visible = visible;
-	}
-
-	public function isVisible():Bool {
-		return this.visible;
 	}
 
 	#if cpp
