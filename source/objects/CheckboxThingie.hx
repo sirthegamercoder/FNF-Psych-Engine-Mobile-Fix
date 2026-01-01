852f4fa6907b569bc48f8a189e98cf39e5701c58
MTGC <satriyoadi130@gmail.com>
2026-01-01 14:05:02 +0700

    what i don't saying about this

--- source/objects/CheckboxThingie.hx
+++ source/objects/CheckboxThingie.hx
@@ -7,28 +7,20 @@ class CheckboxThingie extends FlxSprite
 	public var copyAlpha:Bool = true;
 	public var offsetX:Float = 0;
 	public var offsetY:Float = 0;
-	
 	public function new(x:Float = 0, y:Float = 0, ?checked = false) {
 		super(x, y);
 
 		frames = Paths.getSparrowAtlas('checkboxanim');
-		animation.addByPrefix("unchecked", "checkbox", 24, false);
-		animation.addByPrefix("unchecking", "checkbox anim reverse", 15, false);
-		animation.addByPrefix("checking", "checkbox anim", 15, false);
+		animation.addByPrefix("unchecked", "checkbox0", 24, false);
+		animation.addByPrefix("unchecking", "checkbox anim reverse", 24, false);
+		animation.addByPrefix("checking", "checkbox anim0", 24, false);
 		animation.addByPrefix("checked", "checkbox finish", 24, false);
 
 		antialiasing = ClientPrefs.data.antialiasing;
 		setGraphicSize(Std.int(0.9 * width));
 		updateHitbox();
 
-		if (checked) {
-			animation.play("checked", true);
-			offset.set(3, 12);
-		} else {
-			animation.play("unchecked", true);
-			offset.set(0, 2);
-		}
-		
+		animationFinished(checked ? 'checking' : 'unchecking');
 		animation.finishCallback = animationFinished;
 		daValue = checked;
 	}
@@ -44,15 +36,12 @@ class CheckboxThingie extends FlxSprite
 	}
 
 	private function set_daValue(check:Bool):Bool {
-		var curAnim = animation.curAnim;
-		if (curAnim == null) return check;
-		
 		if(check) {
-			if(curAnim.name != 'checked' && curAnim.name != 'checking') {
+			if(animation.curAnim.name != 'checked' && animation.curAnim.name != 'checking') {
 				animation.play('checking', true);
 				offset.set(34, 25);
 			}
-		} else if(curAnim.name != 'unchecked' && curAnim.name != 'unchecking') {
+		} else if(animation.curAnim.name != 'unchecked' && animation.curAnim.name != 'unchecking') {
 			animation.play("unchecking", true);
 			offset.set(25, 28);
 		}
